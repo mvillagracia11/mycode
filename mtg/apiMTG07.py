cf5815afc4039ad834353d60ecb3620fcb1555ed
MV11 <montriel@users.noreply.github.com>
2022-03-10 23:03:26 +0000

    georecorder

---
+++ mtg/apiMTG07.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Alta3 Research | Author: RZFeeser@alta3.com
+
+   Description:
+   A script to interact with an "open" api,
+   https://api.magicthegathering.io/v1/
+
+   documentation for the API is available via,
+   https://docs.magicthegathering.io/"""
+
+# imports always go at the top of your code
+import requests
+
+# Define our "base" API
+API = "https://api.magicthegathering.io/v1/" # this will never change regardless of the lookup we perform
+
+def main():
+    """Run time code"""
+
+    setcode = input("What is the code of the set you are trying to lookup (see mtgsets.index for a list of codes)? ").lower()   # collect user input for MTG card set to lookup
+
+    # create resp, which is our request object
+    resp = requests.get(f"{API}cards?set={setcode}")   # this "f" string reads: API + "cards/" + setcode
+
+    # the .json() method will dump a JSON string into Pythonic data structures. COOL!
+    # This is much easier than using the urllib.request library
+    cards = resp.json()
+
+    # write data into a file
+    # with open(f"{setcode}_mtg_data.index", "w") as out_file:
+    #    for card in cards:
+    #        print(f"{}")
+
+    # print(cards)
+    # print(cards[0])
+    print(type(cards))
+    print(cards.keys())
+
+
+if __name__ == "__main__":
+    main()
+
